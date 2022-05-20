543b7935de1663752f939bfa36c702bba6bc143a
Marcos QuispeP <84591202+MakeB-rin@users.noreply.github.com>
2022-05-19 23:35:46 -0400

    Add files via upload

---
+++ Ejercicio 4/baraja.R
@@ -0,0 +1,6 @@
+total_simbolos = c("A",2:10,"J","Q","K")
+simbolos = c(rep(total_simbolos,4))
+total_figuras = c("Corazon", "Espada", "Trebol", "Diamante")
+figuras = c(rep(total_figuras,rep(13,4)))
+baraja<-data.frame(simbolos, figuras)
+baraja
