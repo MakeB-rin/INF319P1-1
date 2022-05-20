c777b015b65dba06ea488b24a674f076b558b333
Marcos QuispeP <84591202+MakeB-rin@users.noreply.github.com>
2022-05-19 23:34:05 -0400

    Add files via upload

---
+++ Ejercicio 1/Ordenamiento/Form1.vb
@@ -0,0 +1,88 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a, b, c As Integer
+        a = Convert.ToInt32(TextBox1.Text)
+        b = Convert.ToInt32(TextBox2.Text)
+        c = Convert.ToInt32(TextBox3.Text)
+
+        If a >= b And a >= c Then
+            GoTo a_mayor
+        Else
+            If b >= a And b >= c Then
+                GoTo b_mayor
+            Else
+                If c >= a And c >= b Then
+                    GoTo c_mayor
+                End If
+            End If
+        End If
+
+menor:
+        If a <= b And a <= c Then
+            GoTo a_menor
+        Else
+            If b <= a And b <= c Then
+                GoTo b_menor
+            Else
+                If c <= a And c <= b Then
+                    GoTo c_menor
+                End If
+            End If
+        End If
+        GoTo final
+
+medio:
+        If a >= b And a <= c Or a <= b And a >= c Then
+            GoTo a_medio
+        Else
+            If b >= a And b <= c Or b <= a And b >= c Then
+                GoTo b_medio
+            Else
+                If c >= a And c <= b Or c <= a And c >= b Then
+                    GoTo c_medio
+                End If
+            End If
+        End If
+        GoTo final
+
+a_mayor:
+        TextBox6.Text = a.ToString()
+        GoTo menor
+
+b_mayor:
+        TextBox6.Text = b.ToString()
+        GoTo menor
+
+c_mayor:
+        TextBox6.Text = c.ToString()
+        GoTo menor
+
+a_menor:
+        TextBox4.Text = a.ToString()
+        GoTo medio
+
+b_menor:
+        TextBox4.Text = b.ToString()
+        GoTo medio
+
+c_menor:
+        TextBox4.Text = c.ToString()
+        GoTo medio
+
+a_medio:
+        TextBox5.Text = a.ToString()
+        GoTo final
+
+b_medio:
+        TextBox5.Text = b.ToString()
+        GoTo final
+
+c_medio:
+        TextBox5.Text = c.ToString()
+        GoTo final
+
+
+final:
+    End Sub
+End Class
---
+++ Ejercicio 1/Ordenamiento/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general sobre un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos atributos para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("Ordenamiento")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("Ordenamiento")> 
+<Assembly: AssemblyCopyright("Copyright ©  2022")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
+<Assembly: Guid("8d9c5680-4eef-4572-8307-864c3cc31209")> 
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria 
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión 
+' mediante el asterisco ('*'), como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
